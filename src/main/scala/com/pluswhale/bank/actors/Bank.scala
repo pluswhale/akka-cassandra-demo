ada1afbbea5a91631c4739196948a037f8012e9d
Egor Dultsev <listenmy@yandex.ru>
2022-05-25 15:09:22 +0300

    add some test creating bank and get accounts on main method

--- src/main/scala/com/pluswhale/bank/actors/Bank.scala
+++ src/main/scala/com/pluswhale/bank/actors/Bank.scala
@@ -1,14 +1,21 @@
 package com.pluswhale.bank.actors
 
+import akka.NotUsed
+import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
-import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.{ActorRef, Behavior, Scheduler, ActorSystem}
 import akka.persistence.typed.PersistenceId
 import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
+import akka.util.Timeout
+
 import java.util.UUID
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.DurationInt
 // commands = messages
 import com.pluswhale.bank.actors.PersistentBankAccount.Command
 import PersistentBankAccount.Command._
 import PersistentBankAccount.Response._
+import PersistentBankAccount.Response
 
 
 object Bank {
@@ -61,5 +68,36 @@ object Bank {
       commandHandler = commandHandler(context),
       eventHandler = eventHandler(context)
     )
+  }
+}
+
+//test our bank createAccount and getBankAccount actors
+object BankPlayground {
+  def main(args: Array[String]): Unit = {
+    val rootBehavior: Behavior[NotUsed] = Behaviors.setup { context =>
+      val bank = context.spawn(Bank(), "bank")
+      val logger = context.log
+
+
+      val responseHandler = context.spawn(Behaviors.receiveMessage[Response] {
+        case BankAccountCreatedResponse(id) =>
+          logger.info(s"successfully created bank account $id")
+          Behaviors.same
+        case GetBankAccountResponse(maybeBankAccount) =>
+          logger.info(s"Account details: $maybeBankAccount")
+          Behaviors.same
+      }, "replyHandler")
+      // ask pattern
+      implicit val timeout: Timeout = Timeout(2.seconds)
+      implicit val scheduler: Scheduler = context.system.scheduler
+      implicit val ec: ExecutionContext = context.executionContext
+
+//      bank ! CreateBankAccount("egor", "byn", 100, responseHandler)
+//      bank ! GetBankAccount("faf6bf71-95bb-4417-a12e-32c0e5af1e07", responseHandler)
+
+      Behaviors.empty
+    }
+
+    val system = ActorSystem(rootBehavior, "BankDemo")
   }
 }
